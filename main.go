6ae2daac3996f9fc50e4710531365224c2da468d
Lucas Farias <lukf95@gmail.com>
2024-01-18 10:48:19 +0100

    Add htmx

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -16,19 +17,26 @@ func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	// prod parse all, dev parse on request
-	tmpl := template.Must(template.ParseFiles("./templates/views/index.html", "./templates/components/head.html"))
+	var tmpl *template.Template
+
+	if os.Getenv("APP_ENV") != "development" {
+		tmpl = template.Must(template.ParseFiles("./templates/views/index.html", "./templates/components/head.html"))
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if os.Getenv("APP_ENV") == "development" {
+			tmpl = template.Must(template.ParseFiles("./templates/views/index.html", "./templates/components/head.html"))
+		}
+
 		err := tmpl.Execute(w, map[string]interface{}{
-			"Name": "Cloud",
+			"Name": "Cloud he",
 		})
 		if err != nil {
 			return
 		}
 	})
 
-	fmt.Print("Server running on port 8080")
+	fmt.Println("Server running on port 8080")
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
