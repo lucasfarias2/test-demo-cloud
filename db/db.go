8eaa777f74e104667536b1e899b89cd991e77214
Lucas Farias <lukf95@gmail.com>
2024-02-17 13:16:08 +0100

    Fix?

--- db/db.go
+++ db/db.go
@@ -22,7 +22,7 @@ func ConnectDatabase() {
 
 	log.Printf("Connecting to database: host=%s port=%s", dbHost, dbPort)
 
-	if os.Getenv("APP_ENV") == "production" {
+	if os.Getenv("APP_ENV") != "development" {
 		connectionString = fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable host=/cloudsql/%s", dbUser, dbPassword, dbName, dbUnixSocket)
 	} else {
 		connectionString = fmt.Sprintf("user=%s dbname=%s password=%s host=%s port=%s sslmode=disable", dbUser, dbName, dbPassword, dbHost, dbPort)
