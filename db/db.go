a17bcfab1523a5c62cc4496622c4c4754dda9d96
Lucas Farias <lukf95@gmail.com>
2024-02-17 13:25:54 +0100

    Fix

--- db/db.go
+++ db/db.go
@@ -23,10 +23,10 @@ func ConnectDatabase() {
 
 	if os.Getenv("APP_ENV") != "development" {
 		log.Printf("Connecting to cloud database: host=/cloudsql/%s", dbUnixSocket)
-		connectionString = fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable host=/cloudsql/%s", dbUser, dbPassword, dbName, dbUnixSocket)
+		connectionString = fmt.Sprintf("user=%s password='%s' dbname=%s sslmode=disable host=/cloudsql/%s", dbUser, dbPassword, dbName, dbUnixSocket)
 	} else {
 		log.Printf("Connecting to database: host=%s port=%s", dbHost, dbPort)
-		connectionString = fmt.Sprintf("user=%s dbname=%s password=%s host=%s port=%s sslmode=disable", dbUser, dbName, dbPassword, dbHost, dbPort)
+		connectionString = fmt.Sprintf("user=%s dbname=%s password='%s' host=%s port=%s sslmode=disable", dbUser, dbName, dbPassword, dbHost, dbPort)
 	}
 
 	db, err = sql.Open("postgres", connectionString)
